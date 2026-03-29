65e09cce40ad72968fb5af459d43df0e671ffbbc
Zufar <zufar.info@gmail.com>
2026-03-30 02:34:44 +0500

    remove config.py in gitignore

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    CONFIDENCE_THRESHOLD = 0.25
+    OCCUPIED_THRESHOLD   = 8
+    EMPTY_THRESHOLD      = 12
+    OUTPUT_VIDEO_PATH    = "output.mp4"
+    EVENTS_CSV_PATH      = "events.csv"
+    REPORT_CSV_PATH      = "report.csv"
